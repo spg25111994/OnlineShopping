01ac2c8b8602a6fb19829aad5fdc1f8a71d1bd4f
Saggy <ahirrao.saggy15@gamil.com>
2023-07-21 15:56:00 +0530

    [sagar]adding new class Ajio

---
+++ src/test/java/demo1/Ajio.java
@@ -0,0 +1,11 @@
+package demo1;
+
+import org.testng.annotations.Test;
+
+public class Ajio {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Hello, This is Ajio online service");
+  }
+}
