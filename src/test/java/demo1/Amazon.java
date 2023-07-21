df3b7dc8b622cb30a1f280f87459d8932a38d069
ankit <ankit@DESKTOP-PFBSEA9>
2023-07-21 15:37:44 +0530

    [Shared new project]

---
+++ src/test/java/demo1/Amazon.java
@@ -0,0 +1,13 @@
+package demo1;
+import org.testng.annotations.Test;
+
+public class Amazon {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Hello, This is Amazon online service");
+  }
+    
+	  
+  
+}
---
+++ src/test/java/demo1/Flipkart.java
@@ -0,0 +1,11 @@
+package demo1;
+
+import org.testng.annotations.Test;
+
+public class Flipkart {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Hello, This is Flipkart online service");
+  }
+}
